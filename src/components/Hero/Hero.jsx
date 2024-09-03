052d8989bfa2e0bf539c53ad97d9dbad45796433
Tejaswa <mathurkiit@gmail.com>
2024-09-04 05:15:32 +0530

    uploaded

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Tejaswa</h1>
+        <p className={styles.description}>
+        Hi! I am 4th-year IIT Kanpur student passionate about full-stack development and always eager to explore the latest tech trends
+        </p>
+        <a href="mailto:mathurkiit@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
